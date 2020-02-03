aa7b45fb9d713bfa6d647e30cb573ca81af551c3
JHyuk2 <whdgurdl77@gmail.com>
2020-02-03 19:11:33 +0900

    SWAE D3.py day8 prac

--- SWAC/D3/ppp.py
+++ SWAC/D3/ppp.py
@@ -1,5 +1,5 @@
 tmp =[[True, True, True, True], [True, True, True, True], [True, True, True, True], [True, True, True, True]]
-
+tmp2 = [[True]*4]*4
 
 
 # Q의 좌표를 받았을 때 가로,세로,대각을 False로 만드는 함수
@@ -19,15 +19,15 @@ def turn(tmp, pos):
     return tmp
 
 tmp_list = turn(tmp, (0,0))
-
-
 for i in range(len(tmp_list)):
     for j in range(len(tmp_list)):
         print(tmp_list[i][j], end = ' ')
     print()
 
 print('-'*30)
-for i in range(len(tmp_list)):
-    for j in range(len(tmp_list)):
+
+tmp2_list = turn(tmp2, (0, 0))
+for i in range(len(tmp2_list)):
+    for j in range(len(tmp2_list)):
         print(tmp2_list[i][j], end = ' ')
     print()
--- SWAC/D3/prac2.py
+++ SWAC/D3/prac2.py
@@ -1,4 +1,21 @@
-a = 'a'
-b = 'b'
+tc = int(input())
 
-print('2'<'12')
+for test_case in range(1, tc+1):
+    N, M = map(int, input().split())
+    arr = []
+    
+    for _ in range(N):
+        arr.append(list(map(int, input().split())))
+    print(arr)
+    result = []
+    
+    for x in range(N-M+1):
+        for y in range(N-M+1):
+            group_sum = 0
+            for j in range(M):
+               tmp = arr[x+j][y:y+M] 
+               group_sum += sum(tmp)
+            
+            result.append(group_sum)
+    
+    print('#{} {}'.format(test_case, max(result)))
