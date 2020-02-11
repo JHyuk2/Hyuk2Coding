bf45d7b777c6a814a3515743aaa15ade34f8a24c
JHyuk2 <whdgurdl77@gmail.com>
2020-02-12 01:51:05 +0900

    SWAE 1208 -Flatten [re]

--- SWAC/D3/1208_re.py
+++ SWAC/D3/1208_re.py
@@ -1,27 +1,41 @@
 # 테스트케이스 10
 # 각 줄의 입력은 입력받는 수 & 값
     
-for test_case in range(10):
-    dump = int(input()) # 평탄화 가능회수 입력받음
-    tmp_list = list(map(int, input().split())) 
-    cnt = 0
-	max_num, min_num = 0, 0
-    
+# 덤프 횟수만큼 반복하는데,
+# 맥스값, 민값을 구해서 각 자리에 +1 -1 해주면 됨.
 
-            
-    for i in range(dump):
-        ## 맥스값 찾기.
-        for idx, num in enumerate(tmp_list):
-            if num > maxnum:
+for tc in range(10):
+    n = int(input())
+    d = list(map(int, input().split()))
+    
+    # 덤프횟수
+    for i in range(n):
+        # 각 상황에서의 최대최소
+        max_num = 0
+        min_num = 99999
+    
+        for idx, num in enumerate(d):
+            if num > max_num:
                 max_num = num
                 max_idx = idx
-            elif num < minnum:
+            if num < min_num:
                 min_num = num
                 min_idx = idx
-
-        tmp_list[min_idx] += 1
-        tmp_list[max_idx] -= 1
-        result = tmp_list[max_idx] - tmp_list[min_idx]
+        d[max_idx] -= 1
+        d[min_idx] += 1
     
-    
-    print(f'#{ test_case +1 } { result }')
+    # for문 종료 이후.
+    else:
+        # 마지막으로 한번 더 max min 찾은 다음 반환.
+        max_num = 0
+        min_num = 99999
+        for idx, num in enumerate(d):
+            if num > max_num:
+                max_num = num
+                max_idx = idx
+            if num < min_num:
+                min_num = num
+                min_idx = idx
+        res = d[max_idx] - d[min_idx]
+        
+    print('#{} {}'.format(tc, res))
