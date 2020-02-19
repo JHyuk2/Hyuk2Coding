6061f1f56be216e35202755e0aad4f4541432d88
JHyuk2 <whdgurdl77@gmail.com>
2020-02-19 17:28:05 +0900

    swea 5162 - 빵딜

--- SWAG/D3/5162.py
+++ SWAG/D3/5162.py
@@ -4,4 +4,4 @@ for test_case in range(1, T+1):
     A, B, C = map(int, input().split())
     price = A if A <B else B
     result = C // price
-    print(f'#{test_case} {result}')
+    print(f'#{test_case} {result}') 
